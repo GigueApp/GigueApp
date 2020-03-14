7a92259c71b084b06ccf988b86925f575d830688
Molly Novash <novash@pdx.edu>
2020-03-13 18:48:15 -0700

    Updates

---
+++ precache-manifest.9c8d1ca2bf4827af64d58d8e5986231d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883a5d40fd90e950f1b93746530cb560",
+    "url": "/GigueApp/index.html"
+  },
+  {
+    "revision": "e28b9f6f06b1d2df1cbc",
+    "url": "/GigueApp/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f34fed2ac4004fac8dbe",
+    "url": "/GigueApp/static/css/main.e2d73485.chunk.css"
+  },
+  {
+    "revision": "e28b9f6f06b1d2df1cbc",
+    "url": "/GigueApp/static/js/2.4f30a6a0.chunk.js"
+  },
+  {
+    "revision": "745f4c4ab608042f56da28cfc5e54925",
+    "url": "/GigueApp/static/js/2.4f30a6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f34fed2ac4004fac8dbe",
+    "url": "/GigueApp/static/js/main.f40ef89d.chunk.js"
+  },
+  {
+    "revision": "a3bdad1d23f122ff47d5",
+    "url": "/GigueApp/static/js/runtime-main.964e2c77.js"
+  },
+  {
+    "revision": "1e31c18c30b4f99fa58fecd48275db73",
+    "url": "/GigueApp/static/media/Austine Demo.1e31c18c.ttf"
+  },
+  {
+    "revision": "bb5671edae4b3cecbd3c98159511f2ea",
+    "url": "/GigueApp/static/media/HelveticaNeueLt.bb5671ed.ttf"
+  },
+  {
+    "revision": "5288ef44b14451c3ca2a4a636d1861c6",
+    "url": "/GigueApp/static/media/Ketsana-Regular-PU.5288ef44.ttf"
+  },
+  {
+    "revision": "3ba508d4210940c399f65688b1125916",
+    "url": "/GigueApp/static/media/Kiona-Regular.3ba508d4.ttf"
+  },
+  {
+    "revision": "7cc9b00e5bb9054f28937be1c060b240",
+    "url": "/GigueApp/static/media/Mybela.7cc9b00e.ttf"
+  },
+  {
+    "revision": "f19e02ea7864ad19abd4575363a0759c",
+    "url": "/GigueApp/static/media/Senko Hanabi DEMO.f19e02ea.otf"
+  },
+  {
+    "revision": "230fccca39f830c45b80f2d3b1f33697",
+    "url": "/GigueApp/static/media/Serenity Script.230fccca.otf"
+  },
+  {
+    "revision": "93a94ada6116b9360bf3cf84e44034b7",
+    "url": "/GigueApp/static/media/Warheed.93a94ada.otf"
+  },
+  {
+    "revision": "6040a9618f6f8e60711a8ab836efe84c",
+    "url": "/GigueApp/static/media/mountains.6040a961.jpg"
+  },
+  {
+    "revision": "804900dacb067bde453794de2ac65eb5",
+    "url": "/GigueApp/static/media/ojol.804900da.otf"
+  }
+]);
